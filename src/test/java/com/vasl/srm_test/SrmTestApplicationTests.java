9c42b2a1cd2237f5da7e78b157d10cdbf1c5d70a
mohammad-hossein-ghany <95168706+mohammad-hossein-ghany@users.noreply.github.com>
2025-09-02 11:59:11 +0330

    srm_test template

---
+++ src/test/java/com/vasl/srm_test/SrmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasl.srm_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrmTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
